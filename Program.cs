d18bfea948f4f51a7a68e9f82c936a1f5bda2dbd
lokymine <126381081+lokymine@users.noreply.github.com>
2024-02-09 09:48:30 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1 число:");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("1 число:");
+            int b = Convert.ToInt32(Console.ReadLine());
+            if (a > b)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("a>b");
+            }
+            else if (a < b)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("a<b");
+            }
+            else 
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("a=b");
+            }
+            Console.ReadKey();
+        }
+    }
+}
